6678433664c3259186f710216f87f35f567dd0b2
Rodrigo Vieira <rodrigo.vieira@gmail.com>
2020-11-21 12:17:33 -0300

    Improve checker

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Checador de diários oficiais do Portal da Transparência
 
 Esse crawler é relacionado ao projeto [Querido Diário](https://github.com/okfn-brasil/querido-diario), [issue 353](https://github.com/okfn-brasil/querido-diario/issues/353), e checa quais das cidades disponíveis na plataforma têm diários oficiais ativos: http://atmtec.org.br/#publicacao
+
+Quando for rodar esse crawler o mais importante é checar a variável `MATCH_CRITERIA` em `city_checker_spider.py`, que é como é determinado se os diários oficiais publicados na cidade são recentes.
+
+No momento em que esse checador foi escrito, estamos em Novembro de 2020, por isso `MATCH_CRITERIA` foi setado para `Novembro/2020` pois isso que se espera encontrar na página, por exemplo na cidade de [Areal/RJ](http://rj.portaldatransparencia.com.br/prefeitura/areal/)
+
+Se você quiser fazer alguma alteração e testar rapidamente, use o arquivo `territories_short.csv`, basta trocar o nome do arquivo no spider.
--- check_cities/check_cities/spiders/city_checker_spider.py
+++ check_cities/check_cities/spiders/city_checker_spider.py
@@ -1,21 +1,18 @@
-import scrapy
 import csv
-from unidecode import unidecode
+import scrapy
 from collections import namedtuple
-from scrapy import signals
+from unidecode import unidecode
 
 
 class CityCheckerSpider(scrapy.Spider):
     name = "city_checker"
-    matches = []
-
-    @classmethod
-    def from_crawler(cls, crawler, *args, **kwargs):
-        spider = super(CityCheckerSpider, cls).from_crawler(crawler, *args, **kwargs)
-        crawler.signals.connect(spider.spider_closed, signal=signals.spider_closed)
-        return spider
+    MATCH_CRITERIA = "Novembro/2020"
+    ISSUE_DATE_SELECTOR = "div.data-caderno ::text"
 
     def start_requests(self):
+        with open("result.csv", "w") as result_file:
+            result_file.write("id,state,city,url\n")
+
         for territory in self.load_territories():
             url = f"http://{territory.state}.portaldatransparencia.com.br/prefeitura/{territory.city_uri}"
             yield scrapy.Request(
@@ -23,19 +20,18 @@ class CityCheckerSpider(scrapy.Spider):
             )
 
     def parse(self, response):
-        if response.status == 200 and "Novembro/2020" in response.text:
-            self.matches.append((response.url, response.meta["territory"]))
+        if response.status != 200:
+            return
 
-    def spider_closed(self, spider):
-        spider.logger.info("============== Done! ===============")
-        with open("result.csv", "w") as result_file:
-            result_file.write("id,state,city,url\n")
-            result_file.writelines(
-                [
-                    f"{territory.id},{territory.state.upper()},{territory.city_name},{url}\n"
-                    for url, territory in self.matches
-                ]
-            )
+        issue_date = response.css(self.ISSUE_DATE_SELECTOR).get()
+
+        if issue_date != None and self.MATCH_CRITERIA in issue_date:
+            self.logger.info(f"Success for {response.url}")
+            territory = response.meta["territory"]
+            with open("result.csv", "a") as result_file:
+                result_file.write(
+                    f"{territory.id},{territory.state.upper()},{territory.city_name},{response.url}\n"
+                )
 
     def normalize(self, city):
         return unidecode(city).lower().replace(" ", "")
